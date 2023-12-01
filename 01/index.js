719f0153ba213ef8c303d67a01bf2871aa98bbd2
Patrick Tørresvold <patrick@2rsvold.com>
2023-12-01 08:09:04 +0100

    Make it work...

--- 01/index.js
+++ 01/index.js
@@ -2,19 +2,53 @@ const createLineReader = require('../lib/reader.js');
 const lineReader = createLineReader('input.txt');
 
 // Variables
-let max = 0;
-let current = 0;
+let linedigits = [];
+let verbalDigits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+let verbalStartsWith = 'otfsen';
 
 // Handle lines
 lineReader.onLine((line) => {
-	if (line === '') {
-		if (current > max) max = current;
-		current = 0;
-	} else {
-		current += parseInt(line);
-	}
+	let digits = [];
+
+	// Search for digits
+	line.split('').forEach((d, k) => {
+		if (verbalStartsWith.split('').indexOf(d) !== -1) {
+			let r = lookForVerbalDigit(line, k);
+			if (r != 0) {
+				digits.push(r);
+				console.log("Found word digit:", r);
+			}
+		} else if (parseInt(d)) {
+			digits.push(d);
+			console.log("Found digit: ", d);
+		}
+	});
+
+	// Add digits to
+	linedigits.push(digits[0] + digits[digits.length - 1]);
 });
 
 lineReader.onClose(() => {
-	console.log(max);
+	let sum = linedigits.reduce((a, b) => {
+		return parseInt(a) + parseInt(b);
+	}, 0);
+
+	console.log("Sum: " + sum);
 });
+
+
+function lookForVerbalDigit(line, start) {
+	let result = 0;
+	verbalDigits.forEach((v, k) => {
+		// Check if the first letter of the word is at the starting search point
+		if (v.split('')[0] == line[start]) {
+			let possibleWord = line.split('').splice(start, v.split('').length);
+			if (v == possibleWord.join('')) {
+				console.log("Found a word digit in line", line, ':', v, k + 1);
+				result = (k + 1).toString();
+			}
+		}
+	});
+
+	return result;
+}
