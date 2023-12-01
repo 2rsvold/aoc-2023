0618f4527157bfe39a9077e084ab1d11b5682d17
Patrick Tørresvold <patrick@2rsvold.com>
2023-12-01 08:40:17 +0100

    Cleanup and added nodemon for hotreload

--- 01/index.js
+++ 01/index.js
@@ -1,54 +1,42 @@
 const createLineReader = require('../lib/reader.js');
-const lineReader = createLineReader('input.txt');
+const args = process.argv.slice(2);
+const lineReader = createLineReader(args[0]);
 
 // Variables
-let linedigits = [];
+let digits = [];
 let verbalDigits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-let verbalStartsWith = 'otfsen';
+
+function lookForVerbalDigit(l, s) {
+	let r = 0;
+
+	// Check if the first letter of the word is at the starting search point
+	verbalDigits.forEach((v, k) => {
+		if (v.split('')[0] == l[s]) if (v == l.split('').splice(s, v.split('').length).join('')) r = (k + 1).toString();
+	});
+
+	return r;
+}
 
 // Handle lines
 lineReader.onLine((line) => {
-	let digits = [];
+	let lineDigits = [];
 
-	// Search for digits
+	// Search for digits on each line
 	line.split('').forEach((d, k) => {
-		if (verbalStartsWith.split('').indexOf(d) !== -1) {
+		// Look for the first letter of our words
+		if (verbalDigits.map((v) => v = v.split('')[0]).indexOf(d) !== -1) {
+			// This could be one of the words, try and look for it
 			let r = lookForVerbalDigit(line, k);
-			if (r != 0) {
-				digits.push(r);
-				console.log("Found word digit:", r);
-			}
+			if (r != 0) lineDigits.push(r);
 		} else if (parseInt(d)) {
-			digits.push(d);
-			console.log("Found digit: ", d);
+			// This is a digit we can just add
+			lineDigits.push(d);
 		}
 	});
 
-	// Add digits to
-	linedigits.push(digits[0] + digits[digits.length - 1]);
+	// Add the first and last digits to our memory
+	digits.push(lineDigits[0] + lineDigits[lineDigits.length - 1]);
 });
 
-lineReader.onClose(() => {
-	let sum = linedigits.reduce((a, b) => {
-		return parseInt(a) + parseInt(b);
-	}, 0);
-
-	console.log("Sum: " + sum);
-});
-
-
-function lookForVerbalDigit(line, start) {
-	let result = 0;
-	verbalDigits.forEach((v, k) => {
-		// Check if the first letter of the word is at the starting search point
-		if (v.split('')[0] == line[start]) {
-			let possibleWord = line.split('').splice(start, v.split('').length);
-			if (v == possibleWord.join('')) {
-				console.log("Found a word digit in line", line, ':', v, k + 1);
-				result = (k + 1).toString();
-			}
-		}
-	});
-
-	return result;
-}
+// Sum up all the digits and print on close
+lineReader.onClose(() => console.log("Sum:", digits.reduce((a, b) => parseInt(a) + parseInt(b), 0)));
