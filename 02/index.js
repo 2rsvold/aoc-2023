c19e2f1a80520db756ab2107bd044648dfbee2c7
Patrick Tørresvold <patrick@2rsvold.com>
2023-12-02 13:13:00 +0100

    Added part two

--- 02/index.js
+++ 02/index.js
@@ -8,23 +8,39 @@ const bag = {
 }
 
 let sum = 0;
+let lowest = 0;
 
 lineReader.onLine((line) => {
 	let game = gameAnalyze(line);
 	let isPossible = true;
 
+	let fewest = {
+		red: 0,
+		green: 0,
+		blue: 0
+	}
+
 	game.sets.forEach((e) => {
 		if (e.red > bag.red || e.green > bag.green || e.blue > bag.blue) {
 			isPossible = false;
-			console.log(game.id, 'not possible!');
 		}
+
+		if (e.red > fewest.red) fewest.red = e.red;
+		if (e.green > fewest.green) fewest.green = e.green;
+		if (e.blue > fewest.blue) fewest.blue = e.blue;
 	});
 
+	// Part 1
 	if (isPossible) sum += game.id;
+	
+	// Part 2
+	let power = fewest.red * fewest.green * fewest.blue;
+	lowest += power;
 });
 
 lineReader.onClose(() => {
-	console.log(sum);;
+	console.log("Part one:", sum);
+	console.log("Part two:", lowest);
 });
 
 function gameAnalyze(line) {
@@ -57,18 +73,3 @@ function gameAnalyze(line) {
 
 	return game;
 }
-
-/*
-{
-	id: 1,
-	sets: [[{
-		color: blue,
-		amount: 12
-	}, {
-		color: red,
-		amount: 12
-	}], [
-		co
-	]]
-}
-*/
